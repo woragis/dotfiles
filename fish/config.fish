becaaf3316c979cf0d4970832a94e5230741d1e2
woragis <masteringthecode.woragis+github@gmail.com>
2025-01-26 22:00:11 -0300

    fish: create fish config
    it only has alias for now

---
+++ fish/config.fish
@@ -0,0 +1,54 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Utils alias
+
+# rm alias
+alias rm='rm -I'
+
+# Ls alias
+alias ls='ls -CF --color=auto'
+alias la='ls -A'
+alias lah='ls -Alh'
+
+# Tree alias
+alias tree='tree -FC'
+alias tlea='tree -FCa'
+alias tlee='tree -FCL'
+alias tlea='tree -FCaL'
+
+# Git alias
+alias gs='git status'
+alias ga='git add'
+alias gaa='git add --all'
+alias gc='git commit -m'
+alias gac='git commit -am'
+alias gb='git branch'
+alias gps='git push'
+alias gpl='git pull'
+alias gcl='git clone'
+alias gd='git diff'
+
+# Editor alias
+alias vim='nvim'
+alias v='vim'
+alias nv='vim'
+alias .='nv .'
+
+# Copy alias
+alias cp='cp -i'
+alias df='df -h'
+
+# Grep alias
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+# Coding alias
+alias nrd='npm run dev'
+alias nrb='npm run build'
+
+# Docker alias
+alias dcmp='docker-compose'
+
